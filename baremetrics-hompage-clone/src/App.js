b403f45bbcd4ddc5b01665627e6ac2b08c8a45da
19Kushagra0 <19Kushagra0@gmail.com>
2025-08-13 02:05:47 +0530

    companies using baremetrics container

--- baremetrics-hompage-clone/src/App.js
+++ baremetrics-hompage-clone/src/App.js
@@ -4,6 +4,7 @@ import Header from "./myComponents/1_Header";
 import Title from "./myComponents/2_Title";
 import Payment_providers from "./myComponents/3_Payment_providers";
 import Feature_box1 from "./myComponents/4_Feature_box1";
+import Companies_using from "./myComponents/5_Companies_using ";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Title />
       <Payment_providers />
       <Feature_box1 />
+      <Companies_using />
     </div>
   );
 }
